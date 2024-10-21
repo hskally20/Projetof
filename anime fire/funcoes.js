df434b19c5f78582e7376cd75b7bd7db0f3bbc50
sk <herculesskally81@gmail.com>
2024-10-21 12:22:28 -0300

    DE ZERO 100 EM 6H

---
+++ anime fire/funcoes.js
@@ -0,0 +1,29 @@
+// JavaScript para interações futuras, como carrossel ou funcionalidades adicionais
+
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("Página carregada!");
+  
+    // Função para o botão de pesquisa (exemplo)
+    const searchButton = document.querySelector('button');
+    const searchInput = document.querySelector('input[type="text"]');
+    
+    searchButton.addEventListener('click', function() {
+      const searchQuery = searchInput.value;
+      alert(`Buscando por: ${searchQuery}`);
+      // Aqui você pode adicionar código para buscar animes ou realizar outras ações
+    });
+  });
+
+function toggleGenreList() {
+  const overlay = document.getElementById('genreOverlay');
+  overlay.style.display = 'flex';  // Exibe o overlay
+}
+
+function closeGenreList() {
+  const overlay = document.getElementById('genreOverlay');
+  overlay.style.display = 'none';  // Oculta o overlay
+}
+
+var myCarousel = document.querySelector('#animeCarousel');
+var carousel = new bootstrap.Carousel(myCarousel);
+
