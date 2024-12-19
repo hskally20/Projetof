dbcde4d4d79826c2dab4f8f800dc033b56d9fa7e
sk <herculesskally81@gmail.com>
2024-12-19 09:02:45 -0300

    deu bom

--- anime_project/anime_project/settings.py
+++ anime_project/anime_project/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-        'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
+        
     }
     
 }
